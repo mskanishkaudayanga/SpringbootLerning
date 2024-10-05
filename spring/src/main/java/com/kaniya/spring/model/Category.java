96369fb9364ba1c14b368ceab682da008c90b2e5
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-10-05 20:55:03 +0530

    vsdv

--- spring/src/main/java/com/kaniya/spring/model/Category.java
+++ spring/src/main/java/com/kaniya/spring/model/Category.java
@@ -1,5 +1,6 @@
 package com.kaniya.spring.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,8 +19,9 @@ public class Category {
     private Long id ;
     private String name;
 
-//    @OneToMany(mappedBy = "category")
-//    private List<Product> products;
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 
     public Category(String name) {
         this.name = name;
